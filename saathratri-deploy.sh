cf1fbb84fa25429d68f2a9a08a2249388c7015fe
Amar Premsaran Patel <amarppatel1@gmail.com>
2025-04-02 09:11:04 -0400

    Initial version of workspaces generated by generator-jhipster@8.7.3 with blueprints generator-jhipster-cassandra-composite-primary-key@1.0.8

---
+++ saathratri-deploy.sh
@@ -0,0 +1,21 @@
+cd gateway
+ttab docker compose -f src/main/docker/keycloak.yml up -d
+ttab docker compose -f src/main/docker/jhipster-registry.yml up -d
+cd ..
+
+cd gateway
+npm run docker:db:up
+ttab ./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
+cd ..
+
+cd blog
+npm run docker:db:up
+echo "Deploying Blog Service..."
+ttab ./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
+cd ..
+
+cd store
+npm run docker:db:up
+echo "Deploying Store Service..."
+ttab ./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
+cd ..
---
+++ saathratri-generate-code-dev-cassandra-mf.sh
@@ -0,0 +1,3 @@
+echo "Invoking JHipster generator for development..."
+jhipster --blueprints cassandra-composite-primary-key jdl saathratri-apps-cassandra-mf.jdl --monorepository --workspaces --skip-jhipster-dependencies
+
